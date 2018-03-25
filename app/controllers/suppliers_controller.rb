2c0de4e79c45c3b9da307547aa33db146192892b
Swati Swoboda <swati.swoboda@gmail.com>
2018-03-25 10:52:59 -0400

    moving model logic into named scopes! hey look, class methods!

--- app/controllers/suppliers_controller.rb
+++ app/controllers/suppliers_controller.rb
@@ -3,16 +3,7 @@ class SuppliersController < ApplicationController
 
   # GET /suppliers
   def index
-    @suppliers = Supplier
-      .all
-      .left_joins(:invoices, :invoice_transactions)
-      .group(:id)
-      .order(created_at: :desc)
-      .select(:id, :name, 'count(suppliers.id) AS orders_count, 
-        sum(invoice_transactions.amount) AS amount,
-        sum(CASE WHEN invoices.order_received_date IS NULL THEN invoice_transactions.amount ELSE 0 END) as pending_amount,
-        sum(CASE WHEN invoices.order_received_date IS NOT NULL THEN invoice_transactions.amount ELSE 0 END) as received_amount')
-
+    @suppliers = Supplier.all.transaction_counts
     render json: @suppliers
   end
 
